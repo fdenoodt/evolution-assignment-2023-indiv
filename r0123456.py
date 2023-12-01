8ed855872a86d195ec4c83b8a4029475da66b953
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2023-12-01 14:02:03 +0100

    gradient ascent with dummy example

--- r0123456.py
+++ r0123456.py
@@ -26,33 +26,53 @@ class r0123456:
         f = lambda indiv: (benchmark.compute_fitness(torch.unsqueeze(indiv.clone().detach(), 0))[0])
         self.optimize_plackett_luce(f, self.lr, self.nb_samples_lambda, n)
 
+    def test_loss(self, w_log):
+        # mse with ones vector
+        loss = nn.MSELoss()
+        target = torch.ones_like(w_log)
+        return loss(w_log, target)
+
+
     def optimize_plackett_luce(self, fitness_function, lr, nb_samples_lambda, n):
         w_log = torch.zeros(n)  # w is w_tilde
+        w_log.requires_grad = True
+
         sigma_best = torch.zeros(n)  # the best permutation so far
         best_fitness = torch.inf
 
         ctr = 0
         while True:
-            sigmas = torch.zeros((nb_samples_lambda, n), dtype=torch.int)
-            fitnesses = torch.zeros(nb_samples_lambda)
+            w_log.grad = None
+            loss = self.test_loss(w_log)
+            loss.backward()
 
-            for i in range(nb_samples_lambda):
-                # sample sigma_i from Plackett luce
-                sigmas[i] = self.pl.sample_permutation(torch.exp(w_log))
-                fitnesses[i] = fitness_function(sigmas[i])
+            with torch.no_grad():
+                w_log -= lr * w_log.grad
+                # w_log = torch.clamp(w_log, min=0)
 
-                if fitnesses[i] < best_fitness:
-                    best_fitness = fitnesses[i]
-                    sigma_best = sigmas[i]
+            print(w_log)
 
-            # w_log = ...
 
-            avg_fitness = torch.mean(fitnesses)
-            self.utility.print_score(ctr, best_fitness, avg_fitness, nb_samples_lambda)
-            self.utility.print_array((w_log), ctr, frequency=10)
+
+            # sigmas = torch.zeros((nb_samples_lambda, n), dtype=torch.int)
+            # fitnesses = torch.zeros(nb_samples_lambda)
+            #
+            # for i in range(nb_samples_lambda):
+            #     # sample sigma_i from Plackett luce
+            #     sigmas[i] = self.pl.sample_permutation(torch.exp(w_log))
+            #     fitnesses[i] = fitness_function(sigmas[i])
+            #
+            #     if fitnesses[i] < best_fitness:
+            #         best_fitness = fitnesses[i]
+            #         sigma_best = sigmas[i]
+            #
+            # # w_log = ...
+            #
+            # avg_fitness = torch.mean(fitnesses)
+            # self.utility.print_score(ctr, best_fitness, avg_fitness, nb_samples_lambda)
+            # self.utility.print_array((w_log), ctr, frequency=10)
 
             ctr += 1
-
             if self.utility.is_done(ctr):
                 break
 
