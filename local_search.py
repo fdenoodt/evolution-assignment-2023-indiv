4a3e6ee63a044c26a5fdd032888328f0c40e05dc
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2023-12-26 22:33:07 +0100

    only local search on sub_popul

--- local_search.py
+++ local_search.py
@@ -13,18 +13,18 @@ class LocalSearch:
         nb_cities = np.size(population, 1)
 
         # since matrix is not symmetric, we need to check both directions
-        for indiv_idx, indivd in enumerate(
-                population):  # based on: https://dm865.github.io/assets/dm865-tsp-ls-handout.pdf
+        for indiv_idx, indivd in enumerate(population):
+            # based on: https://dm865.github.io/assets/dm865-tsp-ls-handout.pdf
 
-            for i in range(nb_cities):
-                for j in range(0, nb_cities):  # maybe can start at i+1 if we know the matrix is symmetric
+            for i in range(nb_cities - 1):
+                for j in range(0, nb_cities - 1):  # maybe can start at i+1 if we know the matrix is symmetric
                     if i == j:
                         continue
 
                     a = indivd[i]
                     b = indivd[j]
-                    a_next = indivd[(i + 1) % nb_cities]
-                    b_next = indivd[(j + 1) % nb_cities]
+                    a_next = indivd[i + 1]
+                    b_next = indivd[j + 1]
 
                     # take path from a to a_next and b to b_next and sum them
                     current_cost = d[a, a_next] + d[b, b_next]
@@ -33,8 +33,41 @@ class LocalSearch:
                         # reverse path from a_next to b
                         indivd[i + 1:j + 1] = indivd[j:i:-1]
 
+        # Note that the 0 node is not swapped, so we can remove it
         population = population[:, 1:]
+        return population
 
+    @staticmethod
+    def two_opt_fast(population, d, subset_percentage):
+        # https://stackoverflow.com/questions/53275314/2-opt-algorithm-to-solve-the-travelling-salesman-problem-in-python
+        population = population.copy()
+        population = np.hstack((np.zeros((len(population), 1), dtype=int), population))
+        subpopul = population[:int(len(population) * subset_percentage)]
+        nb_cities = np.size(population, 1)
+
+        # since matrix is not symmetric, we need to check both directions
+        for indiv_idx, indivd in enumerate(subpopul):
+            # based on: https://dm865.github.io/assets/dm865-tsp-ls-handout.pdf
+
+            for i in range(nb_cities - 1):
+                for j in range(0, nb_cities - 1):  # maybe can start at i+1 if we know the matrix is symmetric
+                    if i == j:
+                        continue
+
+                    a = indivd[i]
+                    b = indivd[j]
+                    a_next = indivd[i + 1]
+                    b_next = indivd[j + 1]
+
+                    # take path from a to a_next and b to b_next and sum them
+                    current_cost = d[a, a_next] + d[b, b_next]
+                    would_be_cost = d[a, b] + d[a_next, b_next]
+                    if current_cost > would_be_cost:
+                        # reverse path from a_next to b
+                        indivd[i + 1:j + 1] = indivd[j:i:-1]
+
+        # Note that the 0 node is not swapped, so we can remove it
+        population = population[:, 1:]
         return population
 
     @staticmethod
@@ -43,9 +76,9 @@ class LocalSearch:
         population = np.hstack((np.zeros((len(population), 1), dtype=int), population))
         nb_cities = np.size(population, 1)
 
-        # since matrix is not symmetric, we need to check both directions
-        for indiv_idx, indivd in enumerate(
-                population):  # based on: https://dm865.github.io/assets/dm865-tsp-ls-handout.pdf
+        # Since matrix is not symmetric, we need to check both directions
+        for indiv_idx, indivd in enumerate(population):
+            # based on: https://dm865.github.io/assets/dm865-tsp-ls-handout.pdf
 
             for i in range(nb_cities):
                 for j in range(0, nb_cities):  # maybe can start at i+1 if we know the matrix is symmetric
@@ -84,12 +117,14 @@ if __name__ == "__main__":
     population2 = population1.copy()
 
     fitnesses_before = benchmark.compute_fitness(population1)
+    print(f"before: {np.average(fitnesses_before):_.4f}")
+
+    # population2 = LocalSearch.two_opt_swap(population2, benchmark.matrix)
+    population2 = LocalSearch.two_opt_fast(population2, benchmark.matrix, 0.5)
+    fitnesses_after = benchmark.compute_fitness(population2)
+    print(f"fast: {np.average(fitnesses_after):_.4f}")
+
     population1 = LocalSearch.two_opt(population1, benchmark.matrix)
     fitnesses_after = benchmark.compute_fitness(population1)
-
-    population2 = LocalSearch.two_opt_swap(population2, benchmark.matrix)
-    print(f"before: {np.average(fitnesses_before):_.4f}")
     print(f"two: {np.average(fitnesses_after):_.4f}")
-    print(f"two swap: {np.average(fitnesses_before) - np.average(fitnesses_after):_.4f}")
-
     assert np.all(np.less_equal(fitnesses_after, fitnesses_before))
