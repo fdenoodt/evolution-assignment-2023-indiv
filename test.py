a8af839346086c748a5214a96bd20c5c061c4c5d
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2023-12-31 12:00:47 +0100

    test w/ async io

--- test.py
+++ test.py
@@ -1,9 +1,39 @@
-import numpy
-from numba import int32, jit
 import asyncio
+import time
 
-@jit(nopython=True)
-def fast_function():
-    return np.zeros(5, dtype=int32)
 
-fast_function()
+def background(f):
+    def wrapped(*args, **kwargs):
+        return asyncio.get_event_loop().run_in_executor(None, f, *args, **kwargs)
+
+    return wrapped
+
+
+@background
+def your_function(argument, other_argument):  # Added another argument
+    time.sleep(argument)
+    print(f"function finished for {argument=} and {other_argument=} \n")
+
+
+def code_to_run_before():
+    print('This runs Before Loop!')
+
+
+def code_to_run_after():
+    print('This runs After Loop!')
+
+
+def main():
+    code_to_run_before()  # Anything you want to run before, run here!
+
+    loop = asyncio.get_event_loop()  # Have a new event loop
+    looper = asyncio.gather(*[
+        your_function(i, 1) for i in range(1, 5)
+    ])  # Run the loop
+    results = loop.run_until_complete(looper)  # Wait until finish
+
+    code_to_run_after()
+
+
+if __name__ == '__main__':
+    main()
